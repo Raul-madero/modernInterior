f52cb5479d34465b45df4571dcf61993167cd789
Raul Madero <r.madero.ramirez@gmail.com>
2023-08-01 19:20:07 -0600

    Se agregó el menu mobile con javaScript

--- build/js/app.js
+++ build/js/app.js
@@ -1,2 +1,2 @@
-
+const menu=document.querySelector(".menu-hamburguesa"),cerrar=document.querySelector(".cerrar-menu"),mobileMenu=document.querySelector(".menu_mobile");function abrirMenu(){mobileMenu.classList.toggle("hidden")}function cerrarMenu(){mobileMenu.classList.add("hidden")}window.addEventListener("resize",cerrarMenu),menu.addEventListener("click",abrirMenu),cerrar.addEventListener("click",cerrarMenu);
 //# sourceMappingURL=app.js.map
